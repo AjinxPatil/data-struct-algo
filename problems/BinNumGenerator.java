e9e78a8800de7a609a2c0d6edadbd486fb445570
AjinxPatil <Ajinx.Patil@gmail.com>
2016-03-19 10:28:19 -0700

    Improved code quality of BinarySearch. Added improved method for BinNumGenerator with O(n) time complexity.

--- problems/BinNumGenerator.java
+++ problems/BinNumGenerator.java
@@ -1,25 +1,52 @@
 package problems;
+
+/**
+ * <code>BinNumGenerator</code> generates sequence of binary numbers.
+ * 
+ * @author Ajinkya Patil
+ */
 public class BinNumGenerator {
-  public static StringBuffer[] generate(int n) {
-    StringBuffer[] bins = new StringBuffer[n];
-    bins[0] = new StringBuffer("0");
-    bins[1] = new StringBuffer("1");
-    int count = 1;
-    int i = 2;
-    int t = 2;
-    while (count < n && t < n) {
-      bins[t] = bins[t].append(bins[i - 1]).append("0");
-      bins[t + 1] = bins[t + 1].append(bins[i - 1]).append("1");
-      t = t + 2;
-      i++;
-      count++;
-    }
-    return bins;
-  }
-  public static void main(String[] args) {
-    StringBuffer[] result = BinNumGenerator.generate(Integer.parseInt(args[0]));
-    for (int i = 0; i < result.length; i++) {
-      System.out.println(result[i].toString());
-    }
-  }
+	/**
+	 * Generates sequence of <code>n</code> binary numbers starting from 0. Time
+	 * complexity is O(n).
+	 * 
+	 * @param n
+	 *          the number of binary numbers to generate
+	 * @return array of binary numbers
+	 */
+	public static StringBuffer[] generate(int n) {
+		StringBuffer[] bins = new StringBuffer[n];
+		StringBuffer ZERO = new StringBuffer("0");
+		StringBuffer ONE = new StringBuffer("1");
+		if (n == 1) {
+			bins[0] = ZERO;
+			return bins;
+		}
+		bins[0] = ZERO;
+		bins[1] = ONE;
+		if (n == 2) {
+			return bins;
+		}
+		int sel = 1;
+		int gen = 2;
+		boolean isZero = true;
+		while (gen < n) {
+			if (isZero) {
+				bins[gen] = new StringBuffer().append(bins[sel]).append(ZERO);
+				isZero = false;
+			} else {
+				bins[gen] = new StringBuffer().append(bins[sel++]).append(ONE);
+				isZero = true;
+			}
+			gen++;
+		}
+		return bins;
+	}
+
+	public static void main(String[] args) {
+		StringBuffer[] result = BinNumGenerator.generate(Integer.parseInt(args[0]));
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i].toString());
+		}
+	}
 }
--- problems/BinarySearch.java
+++ problems/BinarySearch.java
@@ -1,37 +1,47 @@
 package problems;
-import java.io.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Binary Search
+ * 
+ * @author Ajinkya Patil
+ */
 public class BinarySearch {
-  public static int search(int[] arr, int start, int end, int x) {
-      int mid = (start + end) / 2;
-      int pos = -1;
-      if (x == arr[mid]) {
-        return mid;
-      }
-      if (start == end) {
-        return pos;
-      }
-      if (x < arr[mid]) {
-        pos = search(arr, start, mid - 1, x);
-      } else {
-        pos = search(arr, mid + 1, end, x);
-      }
-      return pos;
-  }
+	public static int search(int[] a, int start, int end, int x) {
+		int mid = (start + end) / 2;
+		int pos = -1;
+		if (x == a[mid]) {
+			return mid;
+		}
+		if (start == end) {
+			return pos;
+		}
+		if (x < a[mid]) {
+			pos = search(a, start, mid - 1, x);
+		} else {
+			pos = search(a, mid + 1, end, x);
+		}
+		return pos;
+	}
 
-  public static void main(String[] args) throws IOException {
-    int[] arr = new int[args.length];
-    for (int i = 0; i < args.length; i++) {
-      arr[i] = Integer.parseInt(args[i]);
-    }
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    System.out.print("Search: ");
-    int x = Integer.parseInt(in.readLine());
-    int pos = BinarySearch.search(arr, 0, arr.length - 1, x);
-    if (pos < 0) {
-      System.out.println(x + " not found!");
-    } else {
-      System.out.println(x + " found at " + pos);
-    }
-  }
+	public static void main(String[] args) throws IOException {
+		int[] a = new int[args.length];
+		for (int i = 0; i < args.length; i++) {
+			a[i] = Integer.parseInt(args[i]);
+		}
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Search: ");
+		int x = Integer.parseInt(in.readLine());
+		Arrays.sort(a);
+		int pos = BinarySearch.search(a, 0, a.length - 1, x);
+		if (pos < 0) {
+			System.out.println(x + " not found!");
+		} else {
+			System.out.println(x + " found at " + pos);
+		}
+	}
 }
